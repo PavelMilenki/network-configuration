db24eeda0bf910c40bc1cfcbb194ab94def91ff6
Pavel Milenki <54291798+PavelMilenki@users.noreply.github.com>
2020-04-05 18:07:48 +0300

    Updates

---
+++ precache-manifest.56e10c449dba1b6a2ff2232cd545e329.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4472ba96926b82d134f00584620ac9ff",
+    "url": "/network-configuration/index.html"
+  },
+  {
+    "revision": "ba766b6726d7e5e1ca4f",
+    "url": "/network-configuration/static/css/main.efd1e209.chunk.css"
+  },
+  {
+    "revision": "c21cc9fbb882111da572",
+    "url": "/network-configuration/static/js/2.55a40af3.chunk.js"
+  },
+  {
+    "revision": "edfd5a8be4ec1fc32e761a9220464431",
+    "url": "/network-configuration/static/js/2.55a40af3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba766b6726d7e5e1ca4f",
+    "url": "/network-configuration/static/js/main.89b5e57c.chunk.js"
+  },
+  {
+    "revision": "e7a66443149e5d07114c",
+    "url": "/network-configuration/static/js/runtime-main.cacba638.js"
+  }
+]);
